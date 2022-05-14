15ee82de572a9f597c4044012f4953a104fc2234
Chidori <business.faissal@outlook.com>
2022-05-14 12:43:16 -0500

    practicing variables, classes, and data types

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,16 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int a = 2;
+        double b = 2.5;
+        String c = "dog";
+
+        double sum = a + b;
+        String concat = a + c;
+
+        System.out.println(sum);
+        System.out.println(concat);
+
+    }
+}
