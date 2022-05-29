bfa231212f072b9de4ada3120225dc0c143aec92
Chidori <business.faissal@outlook.com>
2022-05-29 12:27:02 -0500

    added css file and began js for upvote functionality

---
+++ src/main/resources/static/js/upvote.js
@@ -0,0 +1,17 @@
+const upvoteClickHandler = async (event) => {
+    event.preventDefault();
+
+    const id = window.location.toString().split('/')[
+    window.location.toString().split('/').length -1
+    ];
+
+    const response = await fetch('/posts/upvote', {
+        method: 'PUT',
+        body: JSON.stringify({
+            postId: id
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+}
